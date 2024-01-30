469c7a0ff473a0b403a163acf24ef222091c4795
alejandroechev <alejandro.echev@gmail.com>
2024-01-30 12:57:11 -0300

    feat: use media query to modify header layout in mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { useMediaQuery } from 'react-responsive'
+
 import Summary from "./Summary"
 import Skills from "./Skills"
 
@@ -5,8 +7,10 @@ import "./Header.css"
 
 export default function Header(props)
 {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
   return (
-    <div className="header">
+    <div className={ isTabletOrMobile ? "header-mobile" : "header"}>
       <img src={props.avatarUrl} alt="User Avatar" className="header-avatar"></img>
       <Summary name={props.name} {...props.profile}/>
       <Skills skills={props.userSkills}/>
